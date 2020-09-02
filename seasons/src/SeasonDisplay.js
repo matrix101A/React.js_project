b4233ea28c131cc51399d6b349be4e55663e4e49
Abhinav <tiwariabhinavin@gmail.com>
2020-09-02 09:46:23 +0530

    Implemented lifecycle method and state variables in js

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import './SeasonDisplay.css'
+const seasonConfig={summer:{text:'Lets hit the beach',iconName:'sun'},winter:{text:'Brr its chilly',iconName:'snowflake'}}
+const getSeason =(lat ,month) =>{
+	if(month>2 && month<9){
+		
+	return lat>0?'summer':'winter';
+	}
+	else
+	{
+		return lat<0?'summer':'winter';
+	}
 
-
-const SeasonDisplay=()=>{
-
-	return(<div>
-		Season
+}
+const SeasonDisplay=props=>{
+const season =getSeason(props.lat,new Date().getMonth());
+const {text,iconName}=seasonConfig[season]
+	return(<div className={`season-display ${season}`}>
+	<i className={`icon-left icon ${iconName} massive`} />
+	<h1>
+		{text}
+		</h1>
+		<i className={`icon-right icon ${iconName} massive`} />
+		
 		</div>
 		);
 };
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,36 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 class App extends React.Component{
   //Constructr to initialize state
-  constructor(props){
-    super(props);
-    this.state={lat:null ,errorMessage:''};
+  state={lat:null , errorMessage:''};
+
+  componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
-      position=>{
-        //we called setState
-        this.setState({lat:position.coords.latitude});
-      },
-      err=>{
-        this.setState({errorMessage:err.message})
-        console.log(err.message)
-        console.log('HE')
-      }
+      position=>  this.setState({lat:position.coords.latitude}),
+      err=>this.setState({errorMessage:err.message})
     );
+    
   }
-  //requirement of react
-  render(){
-      if( this.state.errorMessage && !this.state.lat){
+  renderContent(){
+
+    if( this.state.errorMessage && !this.state.lat){
       return <div>Error :{this.state.errorMessage}</div>;
       }
       if(!this.state.errorMessage && this.state.lat){
-        return <div>Latitude : {this.state.lat}</div>;
+        return<SeasonDisplay
+          lat={this.state.lat}
+        />;
       }
-      return <div>LOADING</div>
+      return <Spinner
+        message="Please accept location request ..."
+      />;
 
   };
-}
+
+  
+  //requirement of react
+  render(){
+      return <div className="border-red">
+       { this.renderContent()}
+      </div>
+}}
 ReactDOM.render(
   <App/>,
   document.querySelector('#root')
