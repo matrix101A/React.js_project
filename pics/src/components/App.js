d2c96154dab4cb07b4ee19f0619806e19f14ec18
Abhinav <tiwariabhinavin@gmail.com>
2020-09-04 18:39:38 +0530

    Started pics project

---
+++ pics/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SerachBar from './SearchBar';
+const App=()=>
+{
+    return (<div className="ui container" style={{marginTop:'10px'}}>
+    <SerachBar/>
+     </div>);
+};
+
+
+export default App
---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+class SearchBar extends React.Component{
+  
+onInputChange(event){
+    console.log(event.target.value);
+   
+}
+
+    render(){
+        return (<div className=" ui segment ">
+        <form className="ui form ">
+            <div className="field">
+            <label >Image Search</label>
+            <input id="inp" type="text" onChange={this.onInputChange} onClick={this.onInputClick}/>
+            </div>
+        </form>
+        </div>);
+    }
+}
+
+export default SearchBar
