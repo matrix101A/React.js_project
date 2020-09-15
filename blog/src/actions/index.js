dd9006304881f78dcc18e7bcf72f4f541772f99e
Abhinav <tiwariabhinavin@gmail.com>
2020-09-15 13:56:57 +0530

    Solved multiple api request problem with memoize function of loadsh liblary

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+const _ = require("lodash");
 
 export const fetchPosts = () => {
   return async (dispatch) => {
@@ -8,9 +9,12 @@ export const fetchPosts = () => {
 };
 
 export const fetchUser = (id) => {
-  return async (dispatch) => {
-    console.log("user");
-    const users = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({ type: "FETCH_USER", payload: users.data });
+  return (dispatch) => {
+    _fetchUser(id, dispatch);
   };
 };
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const users = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({ type: "FETCH_USER", payload: users.data });
+});
