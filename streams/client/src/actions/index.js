1f4dcbe0ef8f11194129a55f2557602ff3630cd8
Abhinav <tiwariabhinavin@gmail.com>
2020-09-20 23:59:56 +0530

    FIXED TYPO

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -21,7 +21,7 @@ export const createStream = (formValues) => async (dispatch) => {
 };
 
 export const fetchStreams = () => async (dispatch) => {
-  const response = await streams.get("/streams", formValues);
+  const response = await streams.get("/streams");
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
